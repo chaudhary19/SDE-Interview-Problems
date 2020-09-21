ea2494c37a5b0984850f10e6b5701456bad37b11
Mayank Chaudhary <49724597+chaudhary19@users.noreply.github.com>
2020-09-22 02:04:30 +0530

    iterative + recursive

---
+++ Z_mergeTwoSortedLinkedList.py
@@ -0,0 +1,44 @@
+# Iterative solution ----------------------------------------------------------
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        
+        dummy = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 or l2
+        
+        return dummy.next
+        
+# Recursive Solution --------------------------------------
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        
+        if not l1 or not l2:
+            return l1 or l2
+        
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+        
